57002f4f39dcd03a54545305bcc8208a3ddb2779
HEKET313 <neket313@gmail.com>
2015-11-11 16:30:35 +0300

    Сделал всплывающие уведомления для формы обратной связи

--- wp-content/themes/tema100/head.php
+++ wp-content/themes/tema100/head.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet/less" type="text/css" href="<?=get_template_directory_uri(); ?>/css/loader.css">
     <link rel="stylesheet/less" type="text/css" href="<?=get_template_directory_uri(); ?>/css/style.less">
     <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/noty/jquery.noty.packaged.min.js"></script>
     <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/less.js" data-env="development"></script>
     <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/modal.js"></script>
     <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/main.js"></script>
--- wp-content/themes/tema100/index/modal_callback.php
+++ wp-content/themes/tema100/index/modal_callback.php
@@ -9,3 +9,48 @@
         <?php echo do_shortcode( '[contact-form-7 id="45" title="Форма обратной связи"]' ); ?>
     </div>
 </div>
+<script type="text/javascript">
+    jQuery(document).ready(function($){
+        var animation = {
+            open: {height: 'toggle'}, // jQuery animate function property object
+            close: {height: 'toggle'}, // jQuery animate function property object
+            easing: 'swing', // easing
+            speed: 500 // opening & closing animation speed
+        };
+        var placement = 'topRight';
+        var timeout = 1500;
+        $(".wpcf7").on('wpcf7:mailsent', function(){
+            noty({
+                text: 'Письмо успешно отправлено! Мы обязательно Вам ответим.',
+                layout: placement,
+                type: 'success',
+                animation: animation,
+                timeout: timeout
+            });
+        }).on('wpcf7:mailfailed', function(){
+            noty({
+                text: 'При отправке письма произошла ошибка! Перезагрузите страницу и попробуйте снова.',
+                layout: placement,
+                type: 'error',
+                animation: animation,
+                timeout: timeout
+            });
+        }).on('wpcf7:spam', function(){
+            noty({
+                text: 'Ваше письмо очень похоже на спам. Измените текст и попробуйте снова.',
+                layout: placement,
+                type: 'warning',
+                animation: animation,
+                timeout: timeout
+            });
+        }).on('wpcf7:invalid', function(){
+            noty({
+                text: 'Не верные параметры отправки.',
+                layout: placement,
+                type: 'error',
+                animation: animation,
+                timeout: timeout
+            });
+        });
+    });
+</script>
